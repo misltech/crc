c0a9fd577e269d05faf58ea0ecf8e7dae994c084
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2020-03-16 00:59:33 -0400

    state form stuff

--- student/myaccount.php
+++ student/myaccount.php
@@ -10,6 +10,9 @@ include_once('components/header.php');
 include_once('components/sidebar.php');
 include_once('components/topnav.php');
 
+function getStates(){
+  include_once('components/state_dropdown.php');
+}
 //validate();
 ?>
 <div class="container-fluid">
@@ -19,16 +22,23 @@ include_once('components/topnav.php');
           <h4 class="mb-3">Edit My Account</h4>
           <form class="needs-validation" novalidate="" _lpchecked="1">
             <div class="row">
-              <div class="col-md-6 mb-3">
+              <div class="col-md-4 mb-3">
                 <label for="firstName">First name</label>
                 <input type="text" class="form-control" id="firstName" placeholder="" value="" required="" style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABHklEQVQ4EaVTO26DQBD1ohQWaS2lg9JybZ+AK7hNwx2oIoVf4UPQ0Lj1FdKktevIpel8AKNUkDcWMxpgSaIEaTVv3sx7uztiTdu2s/98DywOw3Dued4Who/M2aIx5lZV1aEsy0+qiwHELyi+Ytl0PQ69SxAxkWIA4RMRTdNsKE59juMcuZd6xIAFeZ6fGCdJ8kY4y7KAuTRNGd7jyEBXsdOPE3a0QGPsniOnnYMO67LgSQN9T41F2QGrQRRFCwyzoIF2qyBuKKbcOgPXdVeY9rMWgNsjf9ccYesJhk3f5dYT1HX9gR0LLQR30TnjkUEcx2uIuS4RnI+aj6sJR0AM8AaumPaM/rRehyWhXqbFAA9kh3/8/NvHxAYGAsZ/il8IalkCLBfNVAAAAABJRU5ErkJggg==&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%;">
                 <div class="invalid-feedback">
                   Valid first name is required.
                 </div>
               </div>
-              <div class="col-md-6 mb-3">
+              <div class="col-md-4 mb-3">
                 <label for="lastName">Last name</label>
                 <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
+                <div class="invalid-feedback">
+                  Valid last name is required.
+                </div>
+              </div>
+              <div class="col-md-2 mb-3">
+                <label for="lastName">Middle Initial</label>
+                <input type="text" class="form-control" id="lastName" max="1" placeholder="" value="" required="">
                 <div class="invalid-feedback">
                   Valid last name is required.
                 </div>
@@ -36,21 +46,8 @@ include_once('components/topnav.php');
             </div>
 
             <div class="mb-3">
-              <label for="username">Username</label>
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">@</span>
-                </div>
-                <input type="text" class="form-control" id="username" placeholder="Username" required="">
-                <div class="invalid-feedback" style="width: 100%;">
-                  Your username is required.
-                </div>
-              </div>
-            </div>
-
-            <div class="mb-3">
-              <label for="email">Email <span class="text-muted">(Optional)</span></label>
-              <input type="email" class="form-control" id="email" placeholder="you@example.com">
+              <label for="email">Email </label>
+              <input type="email" class="form-control" id="email" value="" placeholder="">
               <div class="invalid-feedback">
                 Please enter a valid email address for shipping updates.
               </div>
@@ -58,37 +55,83 @@ include_once('components/topnav.php');
 
             <div class="mb-3">
               <label for="address">Address</label>
-              <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
+              <input type="text" class="form-control" id="address" value="" required="">
               <div class="invalid-feedback">
                 Please enter your shipping address.
               </div>
             </div>
 
             <div class="mb-3">
-              <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
+              <label for="address2">Address 2</span></label>
               <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
             </div>
 
             <div class="row">
-              <div class="col-md-5 mb-3">
-                <label for="country">Country</label>
-                <select class="custom-select d-block w-100" id="country" required="">
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
+            <div class="col-md-3 mb-3">
+                <label for="zip">City</label>
+                <input type="text" class="form-control" id="zip" placeholder="" required="">
                 <div class="invalid-feedback">
-                  Please select a valid country.
+                  Zip code required.
                 </div>
               </div>
+
               <div class="col-md-4 mb-3">
                 <label for="state">State</label>
-                <select class="custom-select d-block w-100" id="state" required="">
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div class="invalid-feedback">
-                  Please provide a valid state.
-                </div>
+                <select class="custom-select d-block w-100" id="state" value="" required="">
+                <option value=""></option>
+                <option value="AL">Alabama</option>
+                <option value="AK">Alaska</option>
+                <option value="AZ">Arizona</option>
+                <option value="AR">Arkansas</option>
+                <option value="CA">California</option>
+                <option value="CO">Colorado</option>
+                <option value="CT">Connecticut</option>
+                <option value="DE">Delaware</option>
+                <option value="DC">District Of Columbia</option>
+                <option value="FL">Florida</option>
+                <option value="GA">Georgia</option>
+                <option value="HI">Hawaii</option>
+                <option value="ID">Idaho</option>
+                <option value="IL">Illinois</option>
+                <option value="IN">Indiana</option>
+                <option value="IA">Iowa</option>
+                <option value="KS">Kansas</option>
+                <option value="KY">Kentucky</option>
+                <option value="LA">Louisiana</option>
+                <option value="ME">Maine</option>
+                <option value="MD">Maryland</option>
+                <option value="MA">Massachusetts</option>
+                <option value="MI">Michigan</option>
+                <option value="MN">Minnesota</option>
+                <option value="MS">Mississippi</option>
+                <option value="MO">Missouri</option>
+                <option value="MT">Montana</option>
+                <option value="NE">Nebraska</option>
+                <option value="NV">Nevada</option>
+                <option value="NH">New Hampshire</option>
+                <option value="NJ">New Jersey</option>
+                <option value="NM">New Mexico</option>
+                <option value="NY">New York</option>
+                <option value="NC">North Carolina</option>
+                <option value="ND">North Dakota</option>
+                <option value="OH">Ohio</option>
+                <option value="OK">Oklahoma</option>
+                <option value="OR">Oregon</option>
+                <option value="PA">Pennsylvania</option>
+                <option value="RI">Rhode Island</option>
+                <option value="SC">South Carolina</option>
+                <option value="SD">South Dakota</option>
+                <option value="TN">Tennessee</option>
+                <option value="TX">Texas</option>
+                <option value="UT">Utah</option>
+                <option value="VT">Vermont</option>
+                <option value="VA">Virginia</option>
+                <option value="WA">Washington</option>
+                <option value="WV">West Virginia</option>
+                <option value="WI">Wisconsin</option>
+                <option value="WY">Wyoming</option>
+              </select>
+              
               </div>
               <div class="col-md-3 mb-3">
                 <label for="zip">Zip</label>
@@ -97,22 +140,48 @@ include_once('components/topnav.php');
                   Zip code required.
                 </div>
               </div>
+              
             </div>
             
             <hr class="mb-4">
             
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Modify</button>
+            <button class="btn btn-primary btn-lg" type="submit">Modify</button>
           </form>
         </div>
       </div>
 </div>
 <?php
 include_once('components/footer.php');
+include_once('../newback/db_con3.php');
 
+if(isset($_SESSION['email'])){
+  $em = $_SESSION['email'];
+  $sql = "SELECT * FROM s20_student_info WHERE student_email='$em'";
 
+  $result = mysqli_query($db_conn, $sql);
+  if($result){
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $firstname = $row['student_first_name'];
+    $lastname = $row['student_last_name'];
+    $middlename = $row['student_middle_initial'];
+    $email = $row['student_email'];
+    $aptnum = $row['student_apt_num'];
+    $phonenum = $row['student_phone'];
+    $address = $row['student_address'];
+    $city = $row['student_city'];
+    $state = $row['student_state'];
+    $zip = $row['student_zip'];
+  }
+  else{
+    //Something went wrong. Alert in the page somewhere
+  }
 
+  
 
-
+  }
+  
+  
+ 
 
 
 
