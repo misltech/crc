35ed3a0bf5e1f9ce9a2c26e4d57be8c8c4689e04
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2020-03-29 21:02:38 -0400

    simplified queries with inner joins sql

--- admin/workflows.php
+++ admin/workflows.php
@@ -9,11 +9,18 @@ include_once('components/sidebar.php');
 include_once('components/topnav.php');
 ?>
 
+<!-- 
+SELECT concat(student_first_name, ' ' , student_last_name) as Name, s20_application_info.student_email, semester, concat(s20_application_info.dept_code, ' ', class_number) as Course, s20_application_info.instructor_email,  assigned_to, s20_application_info.fw_id
+            FROM s20_application_info 
+            LEFT JOIN s20_student_info ON s20_application_info.banner_id = s20_student_info.banner_id 
+             -->
+
 <!--Table-->
 <div class="container " style="overflow: auto;">
     <div class="jumbotron">
         <h1 class="display-4">View Workflows</h1>
         <p class="lead">You can view and modify current workflows here.</p>
+        <p class="lead danger">No data is found if table is empty!</p>
         <hr class="my-4">
         <table id="example" class="table table-responsive table-striped table-bordered" style="width:100%">
             <thead>
@@ -22,7 +29,6 @@ include_once('components/topnav.php');
                     <th>Student Email</th>
                     <th>Course #</th>
                     <th>Semester</th>
-                    <th>Department Code</th>
                     <th>Faculty Email</th>
                     <th>Assigned to: </th>
                     <th>Modify </th>
@@ -33,67 +39,43 @@ include_once('components/topnav.php');
                 <?php
                 include_once('../newback/db_con3.php');
 
-                $query = 'SELECT * FROM s20_application_info';
-                $run = mysqli_query($db_conn, $query);
-
+                $sql = "SELECT concat(student_first_name, ' ' , student_last_name) as Name, s20_application_info.student_email, semester, concat(s20_application_info.dept_code, ' ', class_number) as Course, s20_application_info.instructor_email, assigned_to, s20_application_info.fw_id FROM s20_application_info LEFT JOIN s20_student_info ON s20_application_info.banner_id = s20_student_info.banner_id";
+                $run = mysqli_query($db_conn, $sql);
+                //alert($run);
                 if (!$run) { //if failed to reach database
                     //errorlog this
                     exit();
-                } else if (mysqli_num_rows($run) == 0) {
-                    //no workflows available
-
-
-                ?>
-                    <!-- no workflow html stuff ehre -->
-
-                    <?php
-
-                } else if (mysqli_num_rows($run) > 0) { //workflows found
-
-                    while ($row = mysqli_fetch_array($run, MYSQLI_ASSOC)) {  //for each email
-                        $studentemail = $row['student_email']; //get email associated with workflow?
-                        $individualstudentquery = "SELECT * FROM s20_student_info WHERE student_email='$studentemail'";
-                        $runstudent = mysqli_query($db_conn, $individualstudentquery);
-
-                        if (!$runstudent) { //if failed to reach database
-                            //errorlog this
-                            exit();
-                        } else if (mysqli_num_rows($runstudent) > 0 and $studentrow = mysqli_fetch_array($runstudent, MYSQLI_ASSOC)) {
-
-
-                            $name = $studentrow['student_first_name'] . " " . $studentrow['student_last_name'];
-                            $email = $studentrow["student_email"];
-                            $course = $row['dept_code'] . " " . $row['class_number'];
-                            $semester = $row['semester'] . " " . $row['year'];
-                            $department = $row['dept_code'];
-                            $facultyemail = $row['instructor_email'];
-                            $assigned = $row['assigned_to'];
-                            $modify = null;
-                    ?>
-
-                            <tr>
-                                <td><?php echo $name; ?></td>
-                                <td><?php echo $email; ?></td>
-                                <td><?php echo $course; ?></td>
-                                <td><?php echo $semester; ?></td>
-                                <td><?php echo $department; ?></td>
-                                <td><?php echo $facultyemail; ?></td>
-                                <td><?php echo $assigned; ?></td>
-                                <td><button><a href="#">Modify</a></button></td>
-                            </tr>
-
-                <?php  }
-                    }
-                } else {
-                    //no idea log this.
                 }
 
+                while ($row = mysqli_fetch_array($run, MYSQLI_ASSOC)) {  //for each email
+                    $name = $row['Name'];
+                    $email = $row["student_email"];
+                    $course = $row['Course'];
+                    $semester = $row['semester'];
+                    $facultyemail = $row['instructor_email'];
+                    $assigned = $row['assigned_to'];
+                    $fwid = $row['fw_id'];
+                    $modify = null;
+                 
                 ?>
+
+                    <tr>
+                        <td><?php echo $name; ?></td>
+                        <td><?php echo $email; ?></td>
+                        <td><?php echo $course; ?></td>
+                        <td><?php echo $semester; ?></td>
+                        <td><?php echo $facultyemail; ?></td>
+                        <td><?php echo $assigned; ?></td>
+                        <td><a class="btn btn-primary btn-block" href="#">Modify</a></button></td>
+                    </tr>
+
+
+                <?php  } ?>
                 </tfoot>
         </table>
     </div>
 </div>
 
-    <?php
-    include_once('components/footer.php');
-    ?>
+<?php
+include_once('components/footer.php');
+?>
