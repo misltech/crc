9f07db88ba324469b80ed4a0dd6f6796c6624df8
Tahir <tahir.mitchell12@guttmanmail.cuny.edu>
2020-04-25 17:02:16 -0400

    secretary can add student

--- secretary/createaccount.php
+++ secretary/createaccount.php
@@ -7,18 +7,18 @@ include_once('../backend/util.php');
 include_once('components/header.php');
 include_once('components/sidebar.php');
 include_once('components/topnav.php');
-
+include_once '../backend/db_con3.php';
 
 //print_r("Email was: " . sendEmail('tahirmitchell@aim.com', "", "This is the internship form"));
 
 ?>
 <div class="container">
   <div class="jumbotron">
-    <h1 class="display-4">Create Account</h1>
+    <h1 class="display-4">Create Accounts</h1>
     <p class="lead">You can generate a single new user, specific to their account type</p>
     <hr class="my-4">
 
-    <nav class="mb-5 nav-pills nav-fill">
+    <nav class="mb-5">
       <ul class="nav nav-tabs" id="myTab" role="tablist">
         <li class="nav-item">
           <a class="nav-link active" id="student-tab" data-toggle="tab" href="#student-body" role="tab" aria-controls="info" aria-selected="true">Student</a>
@@ -37,7 +37,7 @@ include_once('components/topnav.php');
 
     <div class="tab-content" id="createaccounttab">
       <div class="tab-pane  show active" id="student-body" role="tabpanel" aria-labelledby="student-tab">
-        <form>
+        <form method="POST">
           <div class="form-group row">
             <label for="email" class="col-4 col-form-label">Email Address</label>
             <div class="col-8">
@@ -51,17 +51,39 @@ include_once('components/topnav.php');
             </div>
           </div>
           <div class="form-group row">
+            <label for="type" class="col-4 col-form-label">Department Class</label>
+            <div class="col-8">
+              <select id="type" name="utype" class="custom-select" value="" required="required">
+
+                <?php
+                $sql = "SELECT id, dept_code, course_num FROM s20_course_numbers ORDER BY dept_code ASC";
+                $qsql  = mysqli_query($db_conn, $sql);
+                $r = mysqli_num_rows($qsql);
+
+                if ($r > 0) {
+                  while ($result = mysqli_fetch_assoc($qsql)) {
+                    $id = $result['id'];
+                    $deptnum = $result['course_num'];
+                    $deptcode = $result['dept_code'];
+                ?>
+
+                    <option value="<?php showifnotnull($deptcode); ?>"><?php showifnotnull($deptcode . " " . $deptnum); ?></option>
+
+                <?php
+                  }
+                } else {
+                  //form error try again later. redirect
+                }
+                ?>
+
+              </select>
+            </div>
+          </div>
+          <div class="form-group row">
             <label for="type" class="col-4 col-form-label">User Type</label>
             <div class="col-8">
               <select id="type" name="utype" class="custom-select" required="required" disabled>
                 <option value="student">Student</option>
-                <option value="secretary">Secretary</option>
-                <option value="instructor">Instructor</option>
-                <option value="employer">Employer</option>
-                <option value="chair">Department Chair</option>
-                <option value="dean">Dean</option>
-                <option value="admin">Admin</option>
-                <option value="recreg">Records&Registration</option>
               </select>
             </div>
           </div>
@@ -97,7 +119,7 @@ include_once('components/topnav.php');
           </div>
           <div class="form-group row">
             <div class="offset-4 col-8">
-              <button name="submit" type="submit float-right" class="btn btn-primary">Create</button>
+              <button name="submit" id="student_create_button" type="submit float-right" class="btn btn-primary">Create</button>
             </div>
           </div>
         </form>
@@ -176,52 +198,59 @@ include_once('components/topnav.php');
   </div>
 </div>
 
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
+
 <?php
-include_once('components/footer.php');
+
 include_once('../backend/db_con3.php');
 
-if (isset($_POST['submit-student'])) {
-
+if (isset($_POST['submit-student'])) { //handles student submit button
   $email = mysqli_real_escape_string($db_conn, $_POST['email']);
   $banner = mysqli_real_escape_string($db_conn, $_POST['banner']);
   $type = mysqli_real_escape_string($db_conn, $_POST['utype']);
-
   $banner = strtoupper($banner);
 
-
   //check if exists
-  $insert = "INSERT INTO s20_UserPass (banner_id, email, profile_type, passcode) VALUES('$banner', '$email', 'student', " . generatePassword(8) . ")";
-  $qsql  = mysqli_query($db_conn, $insert);
-
+  $pass = generatePassword(8);
+  $insert = "INSERT INTO s20_UserPass (banner_id, email, profile_type, passcode) VALUES('$banner', '$email', 'student', '$pass')";
+  $insertsql  = mysqli_query($db_conn, $insert);
+  
+  
   if (mysqli_errno($db_conn) == 1062) { //mean sduplicate entry
     if (strpos(mysqli_error($db_conn), 'PRIMARY')) { //checks if its in the error line
       //alert that banner id exits
-    } else if (strpos(mysqli_error($db_conn), 'email')) {
+      alert("banner id exist");
+    } else if (strpos(mysqli_error($db_conn), 'email')) { //means duplicated email address. But different banner number
+      alert("email exits");
       //alert that email exist.
-      //ask to send a forgot email
+      //bring up modal. ask to send a forgot email
     }
-  } else if (mysqli_errno($db_conn) == 0) {
-    $newstudentinfosql = "INSERT INTO s20_studentinfo (banner_id, email) VALUES('$banner', '$email')";
-    $insql = mysqli_query($db_conn, $newstudentinfosql);
-    $fwid = bin2hex(random_bytes(32));
-    
-    $newappsql = "INSERT INTO s2_application_info($fw_id,)";
-
-    while(strpos(mysqli_error($db_conn), 'PRIMARY')){ //if dupe found then keep trying till finds an empty fielcwork id. This is unlikely
-
+  } else if (mysqli_errno($db_conn) == 0) { //if user created and query success
+    $fwid = bin2hex(random_bytes(32));  //duplication is unlikely with this one. However should make an method to catch duplication
+    $newappsql = "INSERT INTO s20_application_info(fw_id, banner_id, dept_code, student_email) VALUES ('$fwid','$banner','$type', '$email');"; ///get department code
+    $newappsql .= "INSERT INTO s20_application_util(fw_id, progress, rejected) VALUES ('$fwid', '0', '0');"; 
+    $insql = mysqli_multi_query($db_conn, $newappsql);
+    if(mysqli_errno($db_conn) == 0){
+      alert("success");
     }
-    $newvalidationsql = "";
+    else{
+      alert(mysqli_error($db_conn));
+    }
 
-    
-    $newvalidation ="";
-    $newapp = """;
-    echo "<meta http-equiv='refresh' content='0'>";
+    //echo "<meta http-equiv='refresh' content='0'>";
     //alert success
   }
-
-
-  //$result = mysqli_fetch_assoc($qsql);
-
 }
 
+
+
+
+
+//include_once('components/footer.php');
 ?>
+
+</body>
+</html>
