e6f070f9999f83f7dced944ecda765b77a8401c3
Angel <figueroa10@newpaltz.edu>
2020-05-05 18:02:36 -0400

    revert

--- secretary/createapplication.php
+++ secretary/createapplication.php
@@ -97,17 +97,9 @@ if (isset($_GET['found']) and $_GET['found'] == true) {
                     <label for="uniquesearch">Input a students email address</label>
                     <input id="uniquesearch" name="uniquesearch" type="text" class="form-control" aria-describedby="uniquesearchHelpBlock" required="required">
                     <span id="uniquesearchHelpBlock" class="form-text text-muted">xx@newpaltz.edu</span>
-
-
-                <label for="Semester_Input">Select a Semester</label><br>
-                    <input type="radio" id="Spring" name="semester" value="Spring"> <label for="Spring">Spring</label><br>
-                    <input type="radio" id="Fall"   name="semester" value="Fall">   <label for="Fall">Fall</label><br>
-                    <input type="radio" id="Winter" name="semester" value="Winter"> <label for="Winter">Winter</label><br>
-
                 </div>
 
-
-
+                
                     <button name="submit" type="submit" class="btn btn-primary float-right">Submit Application</button>
                 </div>
             </form>
@@ -135,6 +127,7 @@ if (isset($_GET['found']) and $_GET['found'] == true) {
                             <input id="uniquesearch" name="uniquesearch" type="text" class="form-control" aria-describedby="uniquesearchHelpBlock" required="required">
                             <span id="uniquesearchHelpBlock" class="form-text text-muted">N0365XXXX or xx@newpaltz.edu</span>
                         </div>
+                        
                         <div class="form-group">
                             <button name="search" type="submit" class="btn btn-primary float-right">Search</button>
                         </div>
